0aed31befdbfaaa1a8a5de2b52b4b6876cd36144
401 <401@academicos.cesde.edu.co>
2022-12-06 12:25:17 -0500

    se ponen id

--- controllers/ControllerProducto.js
+++ controllers/ControllerProducto.js
@@ -13,6 +13,41 @@ const crearProducto = async(req, res) => {
   }
 }
 
+const mostrarProductos = async(req, res) =>{
+
+    try {
+      const productos = await Producto.findAll()
+      res.json(productos)
+    } catch (error) {
+      res.json({
+        message: "base de datos vacia"
+      })
+      
+    }
+
+}
+
+const mostrarProducto = async (req, res) => {
+  try {
+    const producto = await Producto.findOne({where: {id: req.params.id}})
+    res.json(producto)
+  } catch (error) {
+    res.json({
+      message: "producto no existe: "+ error
+  })
+
+  }
+}
+
+const editarProducto = async (req, res) => {
+  
+}
+const eliminarProducto = async (req, res) => {
+  
+}
+
 export {
-  crearProducto
+  crearProducto,
+  mostrarProductos,
+  mostrarProducto
 }
--- routes/RouterProducto.js
+++ routes/RouterProducto.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import { crearProducto } from '../controllers/ControllerProducto.js';
+import { crearProducto, mostrarProductos, mostrarProducto } from '../controllers/ControllerProducto.js';
 
 const productoRouter = express.Router();
 
 productoRouter.post('/', crearProducto)
+productoRouter.get('/', mostrarProductos)
+productoRouter.get('/:id', mostrarProducto)
 
 export default productoRouter
